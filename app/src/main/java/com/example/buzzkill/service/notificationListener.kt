ca06f0c1b150c8673487f8da579fae288920f5df
Diffusity <diffusity@gmail.com>
2026-02-21 23:44:59 +0530

    feat: added permissions required in manifest and implemented basic code for notificationListenerService

--- app/src/main/java/com/example/buzzkill/service/notificationListener.kt
+++ app/src/main/java/com/example/buzzkill/service/notificationListener.kt
@@ -1,2 +1,73 @@
 package com.example.buzzkill.service
 
+import android.app.Notification
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import com.example.buzzkill.domain.model.notificationLogs
+import com.example.buzzkill.domain.usecase.addNotificationLogUseCase
+import com.example.buzzkill.domain.usecase.matchRuleUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import jakarta.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class notificationListener: NotificationListenerService(){
+    @Inject lateinit var matchRule: matchRuleUseCase
+    @Inject lateinit var addLog: addNotificationLogUseCase
+
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    override fun onNotificationPosted(sbn: StatusBarNotification){
+        val pkg = sbn.packageName
+        val extras = sbn.notification.extras
+        val title = extras.getString(Notification.EXTRA_TITLE)
+        val body = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString()
+
+        if(pkg == applicationContext.packageName){
+            return
+        }
+
+        serviceScope.launch{
+            val matchRule = matchRule(pkgname = pkg , title = title , body = body)
+
+            if(matchRule != null){
+                applyAction(sbn , matchRule.action)
+            }
+
+            addLog(
+                notificationLogs(
+                    pkgName = pkg,
+                    title = title,
+                    body = body,
+                    appliedRules = matchRule?.name,
+                    action = matchRule?.action,
+                    wasDismissed = matchRule?.action == "DISMISS"
+                )
+            )
+        }
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        super.onNotificationRemoved(sbn)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+
+    private fun applyAction(sbn: StatusBarNotification , action: String){
+        when(action){
+            "DISMISS" -> cancelNotification(sbn.key)
+            "SILENT" -> {}
+            "COOLDOWN" -> {}
+            "REMIND" -> {}
+            "SPEAK" -> {}
+            "SNOOZE" -> {}
+            else -> {}
+        }
+    }
+}
