20b005612becd70d3c0639d8e22cff96a8ee2ab9
Diffusity <diffusity@gmail.com>
2026-02-22 00:33:16 +0530

    viewModel: added code for historyViewModel

--- app/src/main/java/com/example/buzzkill/viewmodel/historyViewModel.kt
+++ app/src/main/java/com/example/buzzkill/viewmodel/historyViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.buzzkill.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.buzzkill.domain.model.notificationLogs
+import com.example.buzzkill.domain.usecase.clearLogsUseCase
+import com.example.buzzkill.domain.usecase.getNotificationLogsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class historyViewModel @Inject constructor(
+    private val getLogs: getNotificationLogsUseCase,
+    private val clearLogs: clearLogsUseCase
+): ViewModel(){
+    val logs: Flow<List<notificationLogs>> = getLogs()
+    fun clearLogs(){
+        viewModelScope.launch {
+            clearLogs.invoke()
+        }
+    }
+}
